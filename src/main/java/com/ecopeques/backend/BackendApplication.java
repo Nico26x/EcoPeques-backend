51873c6fa872e6f10bcc4011d3211d3639401a4e
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2026-09-11 16:59:18 -0500

    Estructura base del proyecto

---
+++ src/main/java/com/ecopeques/backend/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.ecopeques.backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        // Carga las variables del archivo .env al sistema
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
